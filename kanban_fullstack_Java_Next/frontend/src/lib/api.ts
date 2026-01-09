b79733703ff9745ffa97c863d25c707326241443
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-09 20:12:49 +0100

    CreateProjectRequestDTO implement into postProject function in Api.ts

--- kanban_fullstack_Java_Next/frontend/src/lib/api.ts
+++ kanban_fullstack_Java_Next/frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-import { Project } from "../types/projects";
+import CreateProjectRequestDTO, { Project } from "../types/projects";
 import { Task } from "../types/task";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -7,12 +7,12 @@ const TOKEN = process.env.NEXT_PUBLIC_DEV_TOKEN;
 // Pomocnicze nagłówki z tokenem
 const HEADERS = {
   "Content-Type": "application/json",
-  "Authorization": `Bearer ${TOKEN}`,
+  Authorization: `Bearer ${TOKEN}`,
 };
 
 export async function fetchProjectsFromApi() {
   const response = await fetch(`${BASE_URL}/projects`, {
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error("Failed to fetch projects");
   return response.json();
@@ -20,24 +20,24 @@ export async function fetchProjectsFromApi() {
 
 export const fetchProjectFromApi = async (id: string) => {
   const response = await fetch(`${BASE_URL}/projects/${id}`, {
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error("Failed to fetch project");
   return response.json();
 };
 
-export const postProject = async (project: Project) => {
+export const postProject = async (project: CreateProjectRequestDTO) => {
   const response = await fetch(`${BASE_URL}/projects`, {
     method: "POST",
     headers: HEADERS,
     body: JSON.stringify(project),
   });
   if (!response.ok) {
-       const errorData = await response.json();
-       console.error("Błąd z backendu:", errorData);
-       throw new Error("Failed to add project");
-    }
-    return response.json();
+    const errorData = await response.json();
+    console.error("Błąd z backendu:", errorData);
+    throw new Error("Failed to add project");
+  }
+  return response.json();
 };
 
 export const putProject = async (id: string, project: Project) => {
@@ -53,7 +53,7 @@ export const putProject = async (id: string, project: Project) => {
 export const deleteProjectFromApi = async (id: string) => {
   const response = await fetch(`${BASE_URL}/projects/${id}`, {
     method: "DELETE",
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error(`Failed to delete project with id ${id}`);
   return response.status === 204;
@@ -61,7 +61,7 @@ export const deleteProjectFromApi = async (id: string) => {
 
 export async function fetchTasksByProjectId(projectId: string) {
   const response = await fetch(`${BASE_URL}/projects/${projectId}/tasks`, {
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error("Failed to fetch tasks");
   return response.json();
@@ -69,14 +69,14 @@ export async function fetchTasksByProjectId(projectId: string) {
 
 export async function fetchTask(taskId: string) {
   const response = await fetch(`${BASE_URL}/tasks/${taskId}`, {
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error("Failed to fetch task");
   return response.json();
 }
 
 export async function postTask(addedTask: Task) {
-  const response = await fetch(`${BASE_URL}/tasks/${addedTask.projectId}`, {
+  const response = await fetch(`${BASE_URL}/tasks/${addedTask.id}`, {
     method: "POST",
     headers: HEADERS,
     body: JSON.stringify(addedTask), // Poprawiłem: wysyłamy obiekt, nie {addedTask: addedTask}
@@ -88,7 +88,7 @@ export async function postTask(addedTask: Task) {
 export async function deleteTask(taskId: string) {
   const response = await fetch(`${BASE_URL}/tasks/${taskId}`, {
     method: "DELETE",
-    headers: { "Authorization": `Bearer ${TOKEN}` }
+    headers: { Authorization: `Bearer ${TOKEN}` },
   });
   if (!response.ok) throw new Error(`Failed to delete task with id ${taskId}`);
   return response.status === 204;
