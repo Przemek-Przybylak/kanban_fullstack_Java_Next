5095228e328d28c05c8d030807b42273b58c7415
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-09 20:09:46 +0100

    changed task id prop from taskId into id in useTasksStore.ts

--- kanban_fullstack_Java_Next/frontend/src/stores/useTasksStore.ts
+++ kanban_fullstack_Java_Next/frontend/src/stores/useTasksStore.ts
@@ -65,7 +65,7 @@ export const useTasksStore = create<TasksStore>((set, get) => ({
     try {
       await deleteTask(taskId);
       set((state) => ({
-        tasks: state.tasks.filter((task) => task.taskId !== taskId),
+        tasks: state.tasks.filter((task) => task.id !== taskId),
       }));
     } catch (error) {
       set({ error: (error as Error).message });
@@ -77,7 +77,7 @@ export const useTasksStore = create<TasksStore>((set, get) => ({
     set({ loading: true, error: null });
     try {
       // Find the existing task to merge with newData
-      const existingTask = get().tasks.find((task) => task.taskId === taskId);
+      const existingTask = get().tasks.find((task) => task.id === taskId);
       if (!existingTask) throw new Error("Task not found");
       const updatedTask = await putTask(taskId, {
         ...existingTask,
@@ -85,7 +85,7 @@ export const useTasksStore = create<TasksStore>((set, get) => ({
       });
       set((state) => ({
         tasks: state.tasks.map((task) =>
-          task.taskId === taskId ? updatedTask : task
+          task.id === taskId ? updatedTask : task
         ),
       }));
     } catch (error) {
