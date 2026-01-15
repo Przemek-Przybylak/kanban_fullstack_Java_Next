e963993816d772809ae3d3f33319333a7afbb7b7
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-15 17:14:17 +0100

    changed due date from LocaleDateTime to LocalDate in TaskResponseDto

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/DTO/TaskResponseDto.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/DTO/TaskResponseDto.java
@@ -1,8 +1,9 @@
 package com.example.kanban.DTO;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record TaskResponseDto(String id, String title, String description, String status,
-                              String approvedBy, LocalDateTime dueDate, LocalDateTime createdAt,
-                              shortProjectDto project) {
+                              String approvedBy, LocalDate dueDate, LocalDateTime createdAt,
+                              shortProjectDto project, String username) {
 }
