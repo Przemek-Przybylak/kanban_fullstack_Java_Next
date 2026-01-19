9dacd98137d46b5de7b59c4de68c48d8426ee5c4
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-19 20:31:49 +0100

    minimal styles for project title & description added

--- kanban_fullstack_Java_Next/frontend/src/components/Dashboard/Dashboard.tsx
+++ kanban_fullstack_Java_Next/frontend/src/components/Dashboard/Dashboard.tsx
@@ -21,7 +21,32 @@ export default function Dashboard() {
 
   return (
     <div className="p-4 md:p-6 lg:p-8 flex-1 overflow-x-auto">
-      <div>{project?.title}</div>
+      <div className="mb-8">
+        <h1
+          className="
+          text-3xl md:text-4xl
+          font-bold
+          tracking-tight
+          text-slate-900
+          mb-2 uppercase
+        "
+        >
+          {project?.title}
+        </h1>
+
+        {project?.description && (
+          <p
+            className="
+            text-slate-500
+            text-base md:text-lg
+            max-w-2xl
+            leading-relaxed capitalize
+          "
+          >
+            {project.description}
+          </p>
+        )}
+      </div>
       {isOpen && type === "task" && <TaskModal />}
       <CanbanBoard />
     </div>
