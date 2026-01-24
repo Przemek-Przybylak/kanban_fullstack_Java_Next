599bd5316536c3078abc8032901cb90ba05af642
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-24 11:06:34 +0100

    CrossOrigin value added in ProjectController

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/controller/ProjectController.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/controller/ProjectController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/projects")
 public class ProjectController {
     private final ProjectService projectService;
@@ -23,9 +24,9 @@ public class ProjectController {
     }
 
     @GetMapping("/{projectId}/tasks")
-    public ResponseEntity<List<TaskResponseDto>> getTaskByProject(@PathVariable String projectId) {
+    public ResponseEntity<List<TaskResponseDto>> getTasksByProject(@PathVariable String projectId) {
 
-        return ResponseEntity.ok(projectService.getTaskByProject(projectId));
+        return ResponseEntity.ok(projectService.getTasksByProject(projectId));
     }
 
     @PostMapping("/{projectId}/tasks")
