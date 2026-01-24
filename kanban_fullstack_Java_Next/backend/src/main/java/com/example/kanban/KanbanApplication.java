1ee049647d7fe0f742f2ab7e391032ff3bb49a89
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-24 11:03:11 +0100

    basePackages set in KanbanApplication

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/KanbanApplication.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/KanbanApplication.java
@@ -2,8 +2,10 @@ package com.example.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.kanban")
 public class KanbanApplication {
     public static void main(String[] args) {
         SpringApplication.run(KanbanApplication.class, args);
