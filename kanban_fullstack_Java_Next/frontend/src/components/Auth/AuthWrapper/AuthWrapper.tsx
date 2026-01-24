6d5959a5fc86dfcbaf8b59d7aa57f1b24b9d46d8
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-24 11:00:14 +0100

    Auth Wrapper added

---
+++ kanban_fullstack_Java_Next/frontend/src/components/Auth/AuthWrapper/AuthWrapper.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+export default function AuthWrapper() {
+  return (
+    <div className="fixed top-4 right-4 flex gap-4 z-50">
+      <Link href="/auth/login" className="hover:underline">
+        Login
+      </Link>
+      <Link href="/register" className="hover:underline">
+        Register
+      </Link>
+    </div>
+  );
+}
