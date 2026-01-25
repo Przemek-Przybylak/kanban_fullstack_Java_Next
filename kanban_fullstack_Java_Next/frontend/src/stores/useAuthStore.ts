1e5fae18203b7fd8ed4a147bd7efacdce3db6e06
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-25 11:22:25 +0100

    added isLoggedIn flag in auth store

--- kanban_fullstack_Java_Next/frontend/src/stores/useAuthStore.ts
+++ kanban_fullstack_Java_Next/frontend/src/stores/useAuthStore.ts
@@ -6,10 +6,27 @@ type User = {
   username: string;
 };
 
-export const useAuthStore = create<{
-    user: User | mull;
-    setUser: (u: User | null) => void;
-    }>(set => ({
-        user: null,
-        setUser: user => set({ user }),
-        }));
+type AuthState = {
+    user: User | null;
+    isLoggedIn: boolean;
+
+    setUser: (user: User) => void;
+    logout: () => void;
+    }
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  isLoggedIn: false,
+
+  setUser: (user) =>
+    set({
+      user,
+      isLoggedIn: true,
+    }),
+
+  logout: () =>
+    set({
+      user: null,
+      isLoggedIn: false,
+    }),
+}));
