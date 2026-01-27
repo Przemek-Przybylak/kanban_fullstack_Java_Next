24d0b6e9591093405a6ac09c9b98f57e0af02a48
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-27 18:50:49 +0100

    added AdminUserPage component

---
+++ kanban_fullstack_Java_Next/frontend/src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import UsersTable from "../../components/UserTable/UserTable";
+
+import React from "react";
+
+export default function AdminUsersPage() {
+  return (
+    <main className="container mx-auto py-10">
+      <UsersTable />
+    </main>
+  );
+}
