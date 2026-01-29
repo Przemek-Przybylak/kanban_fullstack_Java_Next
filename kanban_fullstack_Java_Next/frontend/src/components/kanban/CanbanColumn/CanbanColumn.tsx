877df0d9af495bebe5ff31fa927b7d9a2fb245fc
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-29 19:22:37 +0100

    use droppable in CanbanColumn implement

--- kanban_fullstack_Java_Next/frontend/src/components/kanban/CanbanColumn/CanbanColumn.tsx
+++ kanban_fullstack_Java_Next/frontend/src/components/kanban/CanbanColumn/CanbanColumn.tsx
@@ -1,25 +1,43 @@
 import React from "react";
+import { useDroppable } from "@dnd-kit/core"; // <--- Musi być ten import
 import { useModalStore } from "../../../stores/useModalStore";
 import { Task } from "../../../types/task";
 import Button from "../../Button/Button";
 import TaskCard from "../TaskCard/TaskCard";
 
-export default function CanbanColumn({ tasks }: { tasks: Task[] }) {
-  const status = tasks.length > 0 ? tasks[0].status : "No tasks";
-  const formattedStatus = status.replace(/_/g, " ").toUpperCase();
+export default function CanbanColumn({
+  tasks,
+  id,
+}: {
+  tasks: Task[];
+  id: string;
+}) {
   const { openModal } = useModalStore();
 
+  const { setNodeRef, isOver } = useDroppable({
+    id: id,
+  });
+
+  const formattedStatus = id.replace(/_/g, " ").toUpperCase();
+
   return (
-    <div className="bg-gray-100 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col">
+    <div
+      ref={setNodeRef}
+      className={`p-6 rounded-lg shadow-md transition-all duration-300 flex flex-col min-w-[300px] ${
+        isOver ? "bg-blue-50 ring-2 ring-blue-300 scale-[1.01]" : "bg-gray-100"
+      }`}
+    >
       <h3 className="text-xl font-semibold mb-4 text-gray-700 border-b border-gray-300 pb-2">
         {formattedStatus}
       </h3>
+
       <Button onClick={() => openModal("addTask")}>Add Task</Button>
-      <div className="flex flex-col gap-4 overflow-y-auto max-h-[70vh]">
+
+      <div className="flex flex-col gap-4 mt-4 overflow-y-auto max-h-[70vh]">
         {tasks.length > 0 ? (
           tasks.map((task) => <TaskCard key={task.id} task={task} />)
         ) : (
-          <p className="text-gray-500 italic">No tasks for this column</p>
+          <p className="text-gray-500 italic py-4 text-center">No tasks here</p>
         )}
       </div>
     </div>
