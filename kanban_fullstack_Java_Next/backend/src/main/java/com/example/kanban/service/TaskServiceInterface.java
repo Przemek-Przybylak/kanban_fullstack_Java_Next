b6af7a0a37a1d7d44fea06112da72839b3268aa3
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-29 19:26:29 +0100

    implement update status interface

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/service/TaskServiceInterface.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/service/TaskServiceInterface.java
@@ -3,6 +3,7 @@ package com.example.kanban.service;
 import com.example.kanban.DTO.TaskPatchRequestDto;
 import com.example.kanban.DTO.TaskRequestDto;
 import com.example.kanban.DTO.TaskResponseDto;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -22,4 +23,8 @@ public interface TaskServiceInterface {
 
     @Transactional
     void deleteTask(String id, String username);
+
+    @Transactional
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('ROLE_ADMIN') or @guard.canAccessProject(#id)")
+    void updateStatus(String taskId, String newStatus);
 }
