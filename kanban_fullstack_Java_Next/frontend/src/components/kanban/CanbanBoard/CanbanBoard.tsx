8c95cbc8efb71bf9e6cf3d59e9783aee107579f0
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-29 19:22:06 +0100

    added dnd sensor in CanbanBoar

--- kanban_fullstack_Java_Next/frontend/src/components/kanban/CanbanBoard/CanbanBoard.tsx
+++ kanban_fullstack_Java_Next/frontend/src/components/kanban/CanbanBoard/CanbanBoard.tsx
@@ -1,8 +1,11 @@
+"use client"; // Ważne w Next.js App Router
+
 import React from "react";
+import { DndContext, DragEndEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { Column } from "../../../types/columns";
 import { Task } from "../../../types/task";
 import CanbanColumn from "../CanbanColumn/CanbanColumn";
-import {useTasksStore} from "../../../stores/useTasksStore";
+import { useTasksStore } from "../../../stores/useTasksStore";
 
 const statusColumn: Column[] = [
   { colId: 1, colTitle: "To Do", value: "todo" },
@@ -12,16 +15,49 @@ const statusColumn: Column[] = [
 ];
 
 export default function CanbanBoard() {
-const {tasks} = useTasksStore();
+  const { tasks, moveTask } = useTasksStore();
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    })
+  );
+
+ const handleDragEnd = (event: DragEndEvent) => {
+   const { active, over } = event;
+
+   console.log("Przeciągany element (active):", active.id);
+   console.log("Upuszczono nad (over):", over?.id);
+
+   if (!over) return;
+
+   const taskId = active.id as string;
+   const newStatus = over.id as string;
+
+   if (taskId && newStatus) {
+     moveTask(taskId, newStatus);
+   }
+ };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 overflow-x-auto p-1 lg:p-0">
-      {statusColumn.map((column) => {
-        const filteredTasks = tasks.filter(
-          (task) => task.status === column.value
-        );
-        return <CanbanColumn key={column.colId} tasks={filteredTasks} />;
-      })}
-    </div>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 overflow-x-auto p-1 lg:p-0">
+        {statusColumn.map((column) => {
+          const filteredTasks = tasks.filter(
+            (task) => task.status === column.value
+          );
+
+          return (
+            <CanbanColumn
+              key={column.colId}
+              id={column.value}
+              tasks={filteredTasks}
+            />
+          );
+        })}
+      </div>
+    </DndContext>
   );
 }
