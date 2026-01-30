7ab2543a5395b82b98447991b78bebbc1c841a01
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-30 05:43:06 +0100

    added basic validation in Input

--- kanban_fullstack_Java_Next/frontend/src/components/Input/Input.tsx
+++ kanban_fullstack_Java_Next/frontend/src/components/Input/Input.tsx
@@ -7,6 +7,7 @@ export default function Input({
   onChange,
   placeholder = "",
   required = false,
+  error = null,
 }: {
   label?: string;
   type?: string;
@@ -14,6 +15,7 @@ export default function Input({
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   required?: boolean;
+  error?: string | null;
 }) {
   return (
     <div className="flex flex-row mb-4">
@@ -26,8 +28,9 @@ export default function Input({
         onChange={onChange}
         placeholder={placeholder}
         required={required}
-        className="border-[1px] border-gray-800 rounded p-2 w-full"
+        className={`border-[1px] border-gray-800 rounded p-2 w-full ${error ? "border-red-500 ring-1 ring-red-500" : "border-gray-300"}`}
       />
+      {error && <span className="text-red-500 text-xs mt-1 font-medium">{error}</span>}
     </div>
   );
 }
