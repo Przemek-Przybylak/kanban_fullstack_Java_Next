8c92ec878630fcab4da7c715b255e91b7851f39e
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-30 21:20:50 +0100

    added textarea to input coponent

--- kanban_fullstack_Java_Next/frontend/src/components/Input/Input.tsx
+++ kanban_fullstack_Java_Next/frontend/src/components/Input/Input.tsx
@@ -1,36 +1,57 @@
 import React from "react";
 
+interface InputProps {
+  label?: string;
+  type?: string;
+  value: string | undefined;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
+  placeholder?: string;
+  required?: boolean;
+  error?: string | null;
+  multiline?: boolean;
+}
+
 export default function Input({
   label = "",
   type = "text",
   value,
   onChange,
-  placeholder = "",
-  required = false,
   error = null,
-}: {
-  label?: string;
-  type?: string;
-  value: string | undefined;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-  required?: boolean;
-  error?: string | null;
-}) {
+  multiline = false,
+  ...props
+}: InputProps) {
+  const baseClasses = `border-[1px] rounded p-2 w-full transition-all outline-none ${
+    error
+      ? "border-red-500 ring-1 ring-red-500"
+      : "border-gray-800 focus:border-blue-500"
+  }`;
+
   return (
-    <div className="flex flex-row mb-4">
+    <div className="flex flex-col mb-4">
       {label && (
         <label className="block text-sm font-medium mb-1">{label}</label>
       )}
-      <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        required={required}
-        className={`border-[1px] border-gray-800 rounded p-2 w-full ${error ? "border-red-500 ring-1 ring-red-500" : "border-gray-300"}`}
-      />
-      {error && <span className="text-red-500 text-xs mt-1 font-medium">{error}</span>}
+
+      {multiline ? (
+        <textarea
+          value={value}
+          onChange={onChange as any}
+          className={baseClasses}
+        />
+      ) : (
+        <input
+          type={type}
+          value={value}
+          onChange={onChange}
+          className={baseClasses}
+        />
+      )}
+
+      {error && (
+        <span className="text-red-500 text-xs mt-1 font-medium">{error}</span>
+      )}
     </div>
   );
 }
