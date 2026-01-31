4992e0cc15fa3d15623ea25c94ed6a68cc487650
Przemek <przemyslaw.przybylak2@gmail.com>
2026-01-31 21:26:31 +0100

    added ErrorResponse record

---
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/exception/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.example.kanban.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        String message,
+        int status
+) {}
