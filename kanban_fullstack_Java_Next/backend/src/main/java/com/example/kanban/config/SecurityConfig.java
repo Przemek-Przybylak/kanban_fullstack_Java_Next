ef88d98b8d927e52ee76253416a6ac701730605e
Przemek <przemyslaw.przybylak2@gmail.com>
2026-02-01 14:25:41 +0100

    build new docker after changes

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/config/SecurityConfig.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,12 +24,13 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(proxyTargetClass = true)
 public class SecurityConfig {
 
     private final JwtCookieFilter jwtCookieFilter;
 
     @Value("${app.jwt.secret}")
-    private String secret; // Zmieniono na private, dobra praktyka
+    private String secret;
 
     public SecurityConfig(JwtCookieFilter jwtCookieFilter) {
         this.jwtCookieFilter = jwtCookieFilter;
