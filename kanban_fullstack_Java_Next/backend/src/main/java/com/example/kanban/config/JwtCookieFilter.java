bbcf7963424ba4557d252afde8dcb10f519c7b40
Przemek <przemyslaw.przybylak2@gmail.com>
2026-02-07 12:24:42 +0100

    fixed: changed config for fix swagger problems in build app

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/config/JwtCookieFilter.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/config/JwtCookieFilter.java
@@ -62,4 +62,12 @@ public class JwtCookieFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/swagger-ui") ||
+                path.startsWith("/v3/api-docs") ||
+                path.startsWith("/auth/");
+    }
 }
