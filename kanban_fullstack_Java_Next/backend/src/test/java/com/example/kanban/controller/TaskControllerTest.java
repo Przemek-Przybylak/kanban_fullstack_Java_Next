8b420bd1613386b8a11d7757a94093003439c8a4
Przemek <przemyslaw.przybylak2@gmail.com>
2026-02-09 05:37:12 +0100

    Tests: added basic mocks to TaskContorller tests

---
+++ kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/controller/TaskControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.kanban.controller;
+
+import com.example.kanban.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TaskController.class)
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+}
