f4e853bcc0c25307cb6611c3eb8b1534235cdfbd
Przemek <przemyslaw.przybylak2@gmail.com>
2026-02-10 20:15:18 +0100

    Testing: added test for post task controller, added assert, for empty description in patch test

--- kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/controller/TaskControllerTest.java
+++ kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/controller/TaskControllerTest.java
@@ -1,11 +1,13 @@
 package com.example.kanban.controller;
 
+import com.example.kanban.DTO.TaskPatchRequestDto;
 import com.example.kanban.DTO.TaskResponseDto;
 import com.example.kanban.model.ProjectRepository;
 import com.example.kanban.model.TaskRepository;
 import com.example.kanban.service.TaskService;
 import com.example.kanban.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -14,12 +16,15 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,17 +47,50 @@ public class TaskControllerTest {
     @MockitoBean
     private TaskRepository taskRepository;
 
+    ArgumentCaptor<TaskPatchRequestDto> taskDtoCaptor = ArgumentCaptor.forClass(TaskPatchRequestDto.class);
+
+    @Test
+    @WithMockUser(username = "test-user", roles = {"USER", "ADMIN"})
+    void shouldUpdateTaskPartially() throws Exception {
+        String taskId = "123";
+
+        when(taskService.editPartialTask(anyString(), any(), anyString()))
+                .thenReturn(new TaskResponseDto(taskId, "New task", "Description", null, null, null, null, null, null));
+
+        mockMvc.perform(patch("/tasks/123", taskId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"New task\"}")
+                        .with(user("test-user").roles("USER"))
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(taskService).editPartialTask(eq("123"), taskDtoCaptor.capture(), eq("test-user"));
+        TaskPatchRequestDto captured = taskDtoCaptor.getValue();
+
+        assertNull(captured.description());
+        assertEquals("New task", captured.title());
+
+
+    }
+
     @Test
     @WithMockUser(username = "test-user", roles = {"USER", "ADMIN"})
     void shouldUpdateTask() throws Exception {
         String taskId = "123";
 
-        when(taskService.editPartialTask(anyString(), any(), anyString()))
-                .thenReturn(new TaskResponseDto(taskId, "Nowe zadanie", "Opis", null, null, null, null, null, null));
+        when(taskService.editTask(anyString(), any(), anyString()))
+                .thenReturn(new TaskResponseDto(taskId, "New task", "Description", null, null, null, null, null, null));
 
-        mockMvc.perform(patch("/tasks/123", taskId)
+        mockMvc.perform(put("/tasks/123", taskId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"Nowe zadanie\"}")
+                        .content("""
+                                {
+                                    "title": "New task",
+                                    "description": "Some description",
+                                    "status": "TODO" 
+                                }
+                                """)
                         .with(user("test-user").roles("USER"))
                         .with(csrf()))
                 .andDo(print())
