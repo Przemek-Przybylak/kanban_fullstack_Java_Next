ba3ae4aecae24fed4a3dc7fac92cef4ad0775693
Przemek <przemyslaw.przybylak2@gmail.com>
2026-02-14 14:59:31 +0100

    testing: added BaseSecurityTest for authorization testing

---
+++ kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/controller/BaseSecurityTest.java
@@ -0,0 +1,29 @@
+package com.example.kanban.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import tools.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public abstract class BaseSecurityTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected void performSecurityCheck(MockHttpServletRequestBuilder requestBuilder) throws Exception {
+        mockMvc.perform(requestBuilder
+                .with(user("intruder").roles("Guest"))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status().isForbidden());
+    }
+}
