4b854252716f960e99a89562b9ad48c57437c3a6
Przemek <przemyslaw.przybylak2@gmail.com>
2026-04-30 22:16:19 +0200

    Testing: test for get all users success added

--- kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/user/controller/UserControllerTest.java
+++ kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/user/controller/UserControllerTest.java
@@ -7,11 +7,17 @@ import com.example.kanban.user.dto.UserResponseDto;
 import com.example.kanban.user.model.Role;
 import com.example.kanban.user.repository.UserRepository;
 import com.example.kanban.user.service.UserService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(AuthController.class)
 public class UserControllerTest {
@@ -34,7 +40,19 @@ public class UserControllerTest {
     @MockitoBean
     private TaskRepository taskRepository;
 
-    UserResponseDto userResponseDto = new UserResponseDto("1", Role.USER, "user");
+    UserResponseDto userResponseDto1 = new UserResponseDto("1", Role.USER, "user1");
+    UserResponseDto userResponseDto2 = new UserResponseDto("2", Role.USER, "user2");
+
+    @Test
+    void shouldGetAllUsers() throws Exception {
+        when(userService.getUsers()).thenReturn(List.of(userResponseDto1, userResponseDto2));
+
+        List<UserResponseDto> result = userService.getUsers();
+
+        assertEquals(2, result.size());
+        assertEquals("user1", result.getFirst().username());
+        assertEquals("user2", result.get(1).username());
+    }
 
 
 }
