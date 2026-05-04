6f7455b67574ad4dac01521717173bbb4482e8ec
Przemek <przemyslaw.przybylak2@gmail.com>
2026-05-04 19:20:07 +0200

    refactoring removed useless userRepository field from UserCotroller

--- kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/user/controller/UserController.java
+++ kanban_fullstack_Java_Next/backend/src/main/java/com/example/kanban/user/controller/UserController.java
@@ -19,11 +19,9 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
-        this.userRepository = userRepository;
     }
 
     @PatchMapping("/{userId}/role")
