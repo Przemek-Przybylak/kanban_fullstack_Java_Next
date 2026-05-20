d84bd5029b6d2adcaaa778eff357092997d4ca16
Przemek <przemyslaw.przybylak2@gmail.com>
2026-05-20 20:23:49 +0200

    Test: added BaseIntegrationTest for use test containers in integration tests

---
+++ kanban_fullstack_Java_Next/backend/src/test/java/com/example/kanban/integration/BaseIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.example.kanban.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("app.jwt.secret", () -> "super_tajny_losowy_klucz_testowy_ktory_ma_ponad_32_znaki_zeby_nie_bylo_bledu");
+    }
+}
